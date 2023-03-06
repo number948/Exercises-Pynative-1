9fbf241e47e8705b0da21d985bf42fde3ed1e1e4
number948 <scottsykotom@gmail.com>
2023-03-05 22:39:39 -0300

    chore: ejercicio 10, list.append was the answer

--- create_New_List.py
+++ create_New_List.py
@@ -1,21 +1,19 @@
 def createNewList():
     list1 = [10, 20, 25, 30, 35]
     list2 = [40, 45, 60, 75, 90]
-    odd_numbers = []
+   
+    result_list = []
+    
     for element in list1:
             if element % 2 != 0:
-                odd_numbers.append(element)
-                print(element)
+                result_list.append(element)
+
     for element_y in list2:
             if element_y % 2 == 0:
-                list3 = element_y
-                print(list3, "\n ")
-
-    new_list = odd_numbers.extend(list3)
-    print(new_list)
-               
-   # ahora hay que juntar odd_numbers + list3, en ese orden , 
-   
+                result_list.append(element_y)
+                
+    print(result_list)
+                  
 
 
 createNewList()
